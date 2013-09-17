a9077e2b2340cee9b2bcb0ccc2f7434c7ffc3827
Thomas Recloux <thomas.recloux@gmail.com>
2013-09-17 22:43:40 +0200

    A real controller test

--- yeoman-maven-jersey-sample/yo/test/spec/controllers/main.js
+++ yeoman-maven-jersey-sample/yo/test/spec/controllers/main.js
@@ -5,6 +5,19 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('yoApp'));
 
+  var books = [
+    {isbn:'11111111', title:'book 1', author:'author 1'},
+    {isbn:'22222222', title:'book 2', author:'author 2'},
+    {isbn:'33333333', title:'book 3', author:'author 3'}
+  ];
+
+  var $httpBackend
+
+  beforeEach(inject(function (_$httpBackend_) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.whenGET('./rest/books').respond(books)
+  }));
+
   var MainCtrl,
     scope;
 
@@ -16,7 +29,9 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach a list of books to the scope', function () {
+    expect(scope.books.length).toBe(0);
+    $httpBackend.flush();
+    expect(scope.books.length).toBe(3);
   });
 });
